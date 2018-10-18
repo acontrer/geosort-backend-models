c8e24305f73114cd0da69290e4b1fe266aa4d8ec
nicolasvasquez <transeunte.123@gmail.com>
2018-10-18 12:34:26 -0300

    Apply database changes

--- models/restrictions.go
+++ models/restrictions.go
@@ -4,4 +4,5 @@ type Restrictions struct {
 	Id                 int `gorm:"column:id;not null;" json:"id" form:"restrictions_id"`
 	TimeWindowsId      int `gorm:"column:time_windows_id;not null;" json:"time_windows_id" form:"restrictions_time_windows_id"`
 	AreaRestrictionsId int `gorm:"column:area_restrictions_id;not null;" json:"area_restrictions_id" form:"restrictions_area_restrictions_id"`
+	DaysId             int `gorm:"column:days_id;not null;" json:"days_id" form:"restrictions_days_id"`
 }
