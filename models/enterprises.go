afc4b959034b733feebdf640c6b2bd3790903587
Daniel Wladdimiro <daniel.wladdimiro@usach.cl>
2018-10-19 11:36:32 -0300

    Add licenses in drivers

--- models/enterprises.go
+++ models/enterprises.go
@@ -1,7 +1,21 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type Enterprises struct {
 	Id   int    `gorm:"column:id;not null;" json:"id" form:"enterprises_id"`
 	Rut  string `gorm:"column:rut;not null;" json:"rut" form:"enterprises_rut"`
 	Name string `gorm:"column:name;not null;" json:"name" form:"enterprises_name"`
+}
+
+func GetEnterpriseByRUT(data *gorm.DB, rutEnterprise string) (int, error) {
+	var enterprise = Enterprises{
+		Rut: rutEnterprise,
+	}
+
+	if err := data.Where(enterprise).Find(&enterprise).Error; err != nil {
+		return enterprise.Id, err
+	} else {
+		return enterprise.Id, nil
+	}
 }
