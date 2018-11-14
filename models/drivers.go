7f0b6dabb9b0699d353622df6059504924339a97
Daniel Wladdimiro <daniel.wladdimiro@usach.cl>
2018-11-14 11:16:01 -0300

    Expand Enterprise in Drivers

--- models/drivers.go
+++ models/drivers.go
@@ -5,16 +5,17 @@ import (
 )
 
 type Drivers struct {
-	Id            int        `gorm:"column:id;not null;" json:"id" form:"drivers_id"`
-	Rut           string     `gorm:"column:rut;not null;" json:"rut" form:"drivers_rut"`
-	FirstName     string     `gorm:"column:first_name;not null;" json:"first_name" form:"drivers_first_name"`
-	LastName      string     `gorm:"column:last_name;not null;" json:"last_name" form:"drivers_last_name"`
-	Phone         string     `gorm:"column:phone;not null;" json:"phone" form:"drivers_phone"`
-	AccountsId    int        `gorm:"column:accounts_id;not null;" json:"accounts_id" form:"drivers_accounts_id"`
-	Account       Accounts   `gorm:"foreignkey:AccountsId;" json:"account" form:"drivers_account"`
-	EnterprisesId int        `gorm:"column:enterprises_id;not null;" json:"enterprises_id" form:"drivers_enterprises_id"`
-	Zones         []Zones    `gorm:"many2many:preferences;" json:"preferences" form:"drivers_zones"`
-	Licenses      []Licenses `gorm:"foreignkey:DriversId;association_foreignkey:Id;" json:"licenses" form:"drivers_licenses"`
+	Id            int         `gorm:"column:id;not null;" json:"id" form:"drivers_id"`
+	Rut           string      `gorm:"column:rut;not null;" json:"rut" form:"drivers_rut"`
+	FirstName     string      `gorm:"column:first_name;not null;" json:"first_name" form:"drivers_first_name"`
+	LastName      string      `gorm:"column:last_name;not null;" json:"last_name" form:"drivers_last_name"`
+	Phone         string      `gorm:"column:phone;not null;" json:"phone" form:"drivers_phone"`
+	AccountsId    int         `gorm:"column:accounts_id;not null;" json:"accounts_id" form:"drivers_accounts_id"`
+	Account       Accounts    `gorm:"foreignkey:AccountsId;" json:"account" form:"drivers_account"`
+	EnterprisesId int         `gorm:"column:enterprises_id;not null;" json:"enterprises_id" form:"drivers_enterprises_id"`
+	Enterprise    Enterprises `gorm:"foreignkey:EnterprisesId;" json:"enterprise" form:"drivers_enterprise"`
+	Zones         []Zones     `gorm:"many2many:preferences;" json:"preferences" form:"drivers_zones"`
+	Licenses      []Licenses  `gorm:"foreignkey:DriversId;association_foreignkey:Id;" json:"licenses" form:"drivers_licenses"`
 }
 
 func (d *Drivers) GetZonesId() []int {
@@ -32,6 +33,10 @@ func (d *Drivers) Expand(data *gorm.DB) error {
 		if err := d.Account.Expand(data); err != nil {
 			return err
 		}
+	}
+
+	if err := data.Model(d).Related(&d.Enterprise).Error; err != nil {
+		return err
 	}
 
 	return nil
