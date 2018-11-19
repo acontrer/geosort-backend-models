8c306203b2a804b8455b9690db7761e75fabcffd
malditoprotozoo <virodriguez.fe@gmail.com>
2018-11-19 12:39:17 -0300

    adding cancellation field to state types

--- models/state_types.go
+++ models/state_types.go
@@ -1,7 +1,8 @@
 package models
 
 type StateTypes struct {
-	Id       int    `gorm:"column:id;not null;" json:"id" form:"state_types_id"`
-	Type     string `gorm:"column:type;not null;" json:"type" form:"state_types_type"`
-	SubState string `gorm:"column:sub_state;not null;" json:"sub_state" form:"state_types_sub_state"`
+	Id           int    `gorm:"column:id;not null;" json:"id" form:"state_types_id"`
+	Type         string `gorm:"column:type;not null;" json:"type" form:"state_types_type"`
+	SubState     string `gorm:"column:sub_state;not null;" json:"sub_state" form:"state_types_sub_state"`
+	Cancellation bool   `gorm:"column:cancellation;not null;" json:"cancellation" form:"state_types_cancellation"`
 }
