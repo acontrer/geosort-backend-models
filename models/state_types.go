119a9655285dd7c3c53d1fc83efa865a91d9d18c
Daniel Wladdimiro <daniel.wladdimiro@usach.cl>
2018-11-21 11:42:45 -0300

    Add Subtype to State Type

--- models/state_types.go
+++ models/state_types.go
@@ -1,8 +1,18 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type StateTypes struct {
-	Id           int    `gorm:"column:id;not null;" json:"id" form:"state_types_id"`
-	Type         string `gorm:"column:type;not null;" json:"type" form:"state_types_type"`
-	SubState     string `gorm:"column:sub_state;not null;" json:"sub_state" form:"state_types_sub_state"`
-	Cancellation bool   `gorm:"column:cancellation;not null;" json:"cancellation" form:"state_types_cancellation"`
+	Id              int           `gorm:"column:id;not null;" json:"id" form:"state_types_id"`
+	Type            string        `gorm:"column:type;not null;" json:"type" form:"state_types_type"`
+	StateSubtypesId string        `gorm:"column:state_subtypes_id;not null;" json:"state_subtypes_id" form:"state_types_state_subtypes_id"`
+	StateSubtype    StateSubtypes `gorm:"foreignkey:StateSubtypesId" json:"state_subtype" form:"state_types_state_subtype"`
+}
+
+func (st *StateTypes) Expand(data *gorm.DB) error {
+	if err := data.Model(st).Related(&st.StateSubtype).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
