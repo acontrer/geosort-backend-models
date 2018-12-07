ffacd647f5e2292bfb45aa43cf7f24dee1792da8
Tori Rodriguez <virodriguez.fe@gmail.com>
2018-12-07 16:56:57 -0300

    correcting column name

--- models/state_subtypes.go
+++ models/state_subtypes.go
@@ -5,7 +5,7 @@ import "github.com/jinzhu/gorm"
 type StateSubtypes struct {
 	Id           int        `gorm:"column:id;not null;" json:"id" form:"state_subtypes_id"`
 	Subtype      string     `gorm:"column:subtype;not null;" json:"subtype" form:"state_subtypes_subtype"`
-	StateTypesId int        `gorm:"column:state_subtypes_id;not null;" json:"state_subtypes_id" form:"state_types_state_subtypes_id"`
+	StateTypesId int        `gorm:"column:state_types_id;not null;" json:"state_types_id" form:"state_types_state_subtypes_id"`
 	Code         string     `gorm:"column:code;not null;" json:"code" form:"state_subtypes_code"`
 	Subcode      string     `gorm:"column:subcode;not null;" json:"subcode" form:"state_types_subcode"`
 	StateTypes   StateTypes `gorm:"foreignkey:StateSubtypesId" json:"state_types" form:"state_subtypes_state_type"`
